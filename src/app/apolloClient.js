1abffeceb5b34ba0bc42dd1c969eaf3819b776f5
Ashu Goyal <ashugoyal@Ashus-MacBook-Pro.local>
2024-11-18 16:51:33 +0530

    backend url updated

--- src/app/apolloClient.js
+++ src/app/apolloClient.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  //uri: "http://localhost:4000/graphql", // Replace with your GraphQL backend URL
-  uri: "https://sprinto-bookhive-1.onrender.com/graphql",
+  // uri: "http://localhost:4000/graphql", // Replace with your GraphQL backend URL
+  uri: "https://bookhive-backend-2s6u.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
